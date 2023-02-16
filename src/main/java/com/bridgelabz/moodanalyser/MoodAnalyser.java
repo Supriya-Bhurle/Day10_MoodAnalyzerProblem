86964e1c1663981f376e2c42b706aec1e66532c8
Supriya Bhurle <supriyabhurle28@gmail.com>
2023-02-16 16:40:26 +0530

    Added : Ability to analyze mood and responce

--- src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalyser.java
@@ -1,2 +1,24 @@
-package com.bridgelabz.moodanalyser;public class MoodAnalyser {
-}
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalyser {
+    private String message;
+
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        if (message.contains("Happy") || message.contains("Any"))
+            return "Happy";
+        else
+            return "Sad";
+    }
+    public String checkMood(String mood) {
+        if (mood.equals("Happy"))
+            return "Happy";
+        else
+            return "Sad";
+    }}
